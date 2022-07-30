34da328187223f55b25ea94a1223f1b3184bf0f1
fhidesjoseph <fhidesjoseph@gmail.com>
2022-07-30 23:21:14 +0300

    commit

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,2 +1,13 @@
 import React,{ useState, useEffect} from 'react';
 import Task from './Task'
+
+const Tasks = ({onDelete,onToggle }) => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/data")
+          .then((r) => r.json())
+          .then((data) => {
+            setTasks(data);
+          });
+      }, [tasks])
