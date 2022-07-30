2dd93b661d0d4b3cb20e5a73eb0754753be9b56a
fhidesjoseph <fhidesjoseph@gmail.com>
2022-07-30 23:31:18 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 function App() {
+  const [showAddTask, setShowAddTask] = useState();
+  
+
+  const switchButton =() =>{
+    setShowAddTask(showAddTask => !showAddTask)
+  }
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>task tracker</h1>
     </div>
   );
